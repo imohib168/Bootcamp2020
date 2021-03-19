113a0c9c951fe40ba88bef8a2fd9e1b95d5b7cd0
Mohib Ismail <imohib168@gmail.com>
2021-03-19 23:19:08 +0500

    Changes in SW

--- todo-ts/public/serviceWorker.js
+++ todo-ts/public/serviceWorker.js
@@ -3,13 +3,13 @@ console.log('Registered');
 const CACHE_NAME = "appV1";
 const urlsToCache = [
     '/',
-    'index.html',
+    '/index.html',
     '/asset-manifest.json',
     'images/logo.png',
     '/manifest.json',
-    '/static/js/bundle.js',
-    '/static/js/vendors~main.chunk.js',
-    '/static/js/main.chunk.js',
+    // '/static/js/bundle.js',
+    // '/static/js/vendors~main.chunk.js',
+    // '/static/js/main.chunk.js',
     '/static/media/bg.00ecbe2e.jpg'
     // 'static/media/cover-min.f6a21fa1.jpg',
     // 'static/js/2.a0125549.chunk.js',
